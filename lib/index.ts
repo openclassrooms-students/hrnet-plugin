3c648aaa74fc669e598bdd23e2d3c2d89697a01d
mamadykonte <contactmamadykonte@gmail.com>
2024-03-05 16:50:28 +0100

    export Calendar

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 import "./index.css";
 import DatePicker from "./components/date-picker/DatePicker";
+import Calendar from "./components/date-picker/Calendar";
 import Modal from "./components/Modal";
 
-export { DatePicker, Modal };
+export { DatePicker, Calendar, Modal };
